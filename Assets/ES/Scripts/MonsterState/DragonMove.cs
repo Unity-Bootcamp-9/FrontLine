73f1d3565fbb64de50dfc6082890e4a637ab501d
shirping1 <a4121240@naver.com>
2024-07-17 17:42:02 +0900

    AR-23 적 이동 상태 구현
    이동 상태에서 몬스터는 무작위 방향으로 움직인다.
    이동이 완료되면 추격 상태로 전이된다.

--- Assets/ES/Scripts/MonsterState/DragonMove.cs
+++ Assets/ES/Scripts/MonsterState/DragonMove.cs
@@ -1,36 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class DragonMove : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    private Monster monster;
+    private Vector3 moveVector;
+    private float timeCount;
+    private float moveDuration;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        monster = animator.GetComponent<Monster>();
+        moveVector = new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.1f, 0.1f), Random.Range(-0.3f, 0.3f));
+        timeCount = 0f;
+        moveDuration = Random.Range(0.5f, 1.5f);
+        monster.LerpRotate(monster.transform.position + moveVector, () => monster.SetVelocityMoveSpeed(), 1f);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(moveDuration <= timeCount)
+        {
+            monster.SetAnimator("Chase", true);
+        }
+        timeCount += Time.deltaTime;
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        monster.SetVelocityZero();
+        monster.SetAnimator("Move", false);
+    }
 }
