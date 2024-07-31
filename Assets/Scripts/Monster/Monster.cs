baec6622f188d9511a4378a3a21d90bdb089521e
shirping1 <a4121240@naver.com>
2024-07-31 14:00:57 +0900

    몬스터 투사체 풀 생성
    몬스터 스크립트에 투사체 풀을 가지고 있는다

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Monster : MonoBehaviour, IMonster
 {
@@ -10,15 +11,85 @@ public class Monster : MonoBehaviour, IMonster
     private Animator animator;
     public MonsterData monsterData;
     public Portal portal;
-    private Projectile projectile;
     protected int currentHP;
     public Rigidbody rigidBody;
     [SerializeField] private Transform firePos;
 
+    private static Dictionary<string, IObjectPool<Projectile>> projectiles = new Dictionary<string, IObjectPool<Projectile>>();
+    public Projectile GetProjectiles(string path, Vector3 _startPos, Vector3 target, float duration)
+    {
+        Projectile projectile = null;
+        if (projectiles.ContainsKey(path))
+        {
+            projectile = projectiles[path].Get();
+            projectile.SetTarget(_startPos, target, duration, path);
+            return projectile;
+        }
+
+        projectile = Managers.Resource.Load<Projectile>("MonsterProjectile/" + path);
+        MakeObjectPool(projectile, path);
+        projectile = projectiles[path].Get();
+        projectile.SetTarget(_startPos, target, duration, path);
+        return projectile;
+    }
+
+    #region CreatePool
+    public static void ProjectileReturnToPool(string path, Projectile projectile)
+    {
+        projectiles[path].Release(projectile);
+    }
+
+    private bool collectionChecks = true;
+    private int maxPoolSize = 10;
+
+    private void MakeObjectPool(Projectile prefab, string path)
+    {
+        if (projectiles.ContainsKey(path))
+        {
+            Debug.LogWarning($"Pool for {prefab.name} already exists.");
+            return;
+        }
+
+        IObjectPool<Projectile> pool = new ObjectPool<Projectile>(
+            () => CreateObject(prefab),
+            OnTakeFromPool,
+            OnReturnedToPool,
+            OnDestroyPoolObject,
+            collectionChecks,
+            10,
+            maxPoolSize
+        );
+
+        projectiles.Add(path, pool);
+    }
+
+    private Projectile CreateObject(Projectile prefab)
+    {
+        Projectile projectile = Managers.Resource.Instantiate(prefab);
+        projectile.transform.parent = Managers.Instance.game.transform;
+        return projectile;
+    }
+
+    private void OnTakeFromPool(Projectile obj)
+    {
+        obj.gameObject.SetActive(true);
+    }
+
+    private void OnReturnedToPool(Projectile obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(Projectile obj)
+    {
+        Managers.Resource.Destroy(obj.gameObject);
+    }
+
+    #endregion
+
     private void Start()
     {
         playerPos = Camera.main.transform.position;
-        projectile = Resources.Load<Projectile>("MonsterProjectile/" + monsterData.projectile);
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
     }
@@ -51,10 +122,11 @@ public class Monster : MonoBehaviour, IMonster
     {
         if (monsterData.attackRange >= 20)
         {
-            Projectile newProjectile = Instantiate(projectile);
-            newProjectile.transform.parent = Managers.Instance.game.transform;
-            newProjectile.SetTarget(firePos.position, playerPos, 3f);
-            newProjectile.SetDamage(monsterData.attackDamage);
+            GetProjectiles(monsterData.projectile, firePos.position, playerPos, 3f);
+            //Projectile newProjectile = Instantiate(projectile);
+            //newProjectile.transform.parent = Managers.Instance.game.transform;
+            //newProjectile.SetTarget(firePos.position, playerPos, 3f);
+            //newProjectile.SetDamage(monsterData.attackDamage);
         }
         else
         {
